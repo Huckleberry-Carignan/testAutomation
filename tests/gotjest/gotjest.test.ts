9cab91a5a960c73eb94de68514b40a910979e353
Huckleberry Carignan <huckleberry.carignan@sunrun.com>
2022-10-11 14:35:13 -0400

    trying to get it to work

---
+++ tests/gotjest/gotjest.test.ts
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+import {describe, expect, test} from '@jest/globals';
+
+describe('sum module', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(3).toBe(3);
+    });
+  });
+
+
+  describe('see if got works', () => {
+    test('get a cat fact', async () => {
+        //const foo = await fetch('https://catfact.ninja/fact');
+      expect(3).toBe(3);
+    });
+  });
